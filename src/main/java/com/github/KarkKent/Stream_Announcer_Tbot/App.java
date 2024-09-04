0658e8b7660427091cd0fdcab3d523891183547b
Anton Kulbakov <culbackov@yandex.ru>
2024-09-04 15:47:26 +0200

    Initial commit

---
+++ src/main/java/com/github/KarkKent/Stream_Announcer_Tbot/App.java
@@ -0,0 +1,13 @@
+package com.github.KarkKent.Stream_Announcer_Tbot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
